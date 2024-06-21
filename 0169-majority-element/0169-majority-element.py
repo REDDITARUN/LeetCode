be0eb95f6c4455498ed140b3b5d1ed6b28a84b14
Tarun <53268025+REDDITARUN@users.noreply.github.com>
2024-06-21 04:39:02 -0400

    Time: 157 ms (71.47%), Space: 13.4 MB (36.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -6,15 +6,19 @@ class Solution(object):
         """
         
         
-        store = {}
-        count = len(nums)/2
-        
-        for ele in nums:
-            if ele in store:
-                store[ele] += 1
-            else:
-                store[ele]= 1
-        
-            if store[ele] > count:
-                return ele
+
             
+            
+        candidate = None
+        count = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate

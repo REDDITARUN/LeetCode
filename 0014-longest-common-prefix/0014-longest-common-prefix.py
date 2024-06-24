58394566fc8cf7c721d82b03d492f04fa9b7cdce
Tarun <53268025+REDDITARUN@users.noreply.github.com>
2024-06-24 13:42:48 -0400

    Time: 16 ms (62.88%), Space: 11.7 MB (83.65%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -9,17 +9,33 @@ class Solution(object):
             return ""
         
         
-        prefix = strs[0]
+#         prefix = strs[0]
         
         
-        for s in strs[1:]:
+#         for s in strs[1:]:
             
-            while not s.startswith(prefix):
-                prefix = prefix[:-1]
+#             while not s.startswith(prefix):
+#                 prefix = prefix[:-1]
                 
-                if not prefix:
-                    return ""
+#                 if not prefix:
+#                     return ""
             
+#         return prefix
+
+        min_len = min(len(s) for s in strs)
+
+        
+        prefix = ""
+
+       
+        for i in range(min_len):
+        
+            current_char = strs[0][i]
+            if all(s[i] == current_char for s in strs):
+                prefix += current_char
+            else:
+                break
+
         return prefix
         
         

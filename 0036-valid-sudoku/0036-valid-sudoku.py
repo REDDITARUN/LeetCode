f7e54b3dc4799398558298b6e59af139583d6ff4
Tarun <53268025+REDDITARUN@users.noreply.github.com>
2024-07-11 23:10:56 +0530

    Time: 53 ms (95.81%), Space: 11.7 MB (24.30%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        # Create sets to keep track of seen numbers
+        rows = [set() for _ in range(9)]
+        columns = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+                if num == '.':
+                    continue
+                
+                # Check the row
+                if num in rows[r]:
+                    return False
+                rows[r].add(num)
+                
+                # Check the column
+                if num in columns[c]:
+                    return False
+                columns[c].add(num)
+                
+                # Check the 3x3 sub-box
+                box_index = (r // 3) * 3 + (c // 3)
+                if num in boxes[box_index]:
+                    return False
+                boxes[box_index].add(num)
+        
+        return True
+

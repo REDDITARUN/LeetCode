31fb7a43eeb9df981cf468fb6abf1a16afc98959
Tarun <53268025+REDDITARUN@users.noreply.github.com>
2024-08-04 21:28:43 +0530

    Time: 22 ms (59.92%), Space: 11.6 MB (28.79%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -4,13 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        n = len(nums)
         total = sum(nums)
-        left_sum = 0
+        left = 0
         
-        for i in range(len(nums)):
-            right_sum = total - left_sum - nums[i]
-            if left_sum == right_sum:
+        
+        for i in range(n):
+            right = total - left - nums[i]
+            if right == left:
                 return i
-            left_sum += nums[i]
+            left = left + nums[i]
         
         return -1
+           
+            
+        
+            
+        

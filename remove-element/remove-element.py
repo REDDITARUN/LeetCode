da0280d1ca160fe0a5a2df96cfde03311d02e437
Tarun <53268025+REDDITARUN@users.noreply.github.com>
2024-10-07 13:22:50 -0400

    Time: 45 ms, Memory: 16.6 MB - LeetHub

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -1,13 +1,22 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        l = len(nums)
         k = 0
-        c = 0
-        for i in range(l):
-            if nums[i] == val:
-                nums[i] = 0
-                c += 1
-        nums.sort(reverse=True)
-        k = l - c
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k+=1
         return k
+    
+    
+        # l = len(nums)
+        # k = 0
+        # c = 0
+        # for i in range(l):
+        #     if nums[i] == val:
+        #         nums[i] = 0
+        #         c += 1
+        # nums.sort(reverse=True)
+        # k = l - c
+        # return k
         

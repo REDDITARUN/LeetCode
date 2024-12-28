760f10446c6a1b36f579a34b7bcda9e98d6a21db
Tarun <53268025+REDDITARUN@users.noreply.github.com>
2024-12-28 17:32:49 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (9.28%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -6,11 +6,11 @@ class Solution(object):
         :rtype: int
         """
         
-        # return haystack.find(needle)  # using the find function
-        if not needle:
-            return 0
+        return haystack.find(needle)  # using the find function
+        # if not needle:
+        #     return 0
         
-        for i in range(len(haystack) - len(needle) + 1):
-            if haystack[i: i+len(needle)] == needle:
-                return i
-        return -1
+        # for i in range(len(haystack) - len(needle) + 1):
+        #     if haystack[i: i+len(needle)] == needle:
+        #         return i
+        # return -1

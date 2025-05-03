5d45209e8bf4fd7e6e626af75340d67d90c42ce4
Tarun <53268025+REDDITARUN@users.noreply.github.com>
2025-05-03 12:24:29 -0400

    Time: 292 ms (91.67%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,6 +1,2 @@
-# Write your MySQL query statement below
-SELECT emp.name, bon.bonus
-FROM 
-Employee emp
-LEFT JOIN Bonus bon ON emp.empId = bon.empId
-WHERE bon.bonus < 1000 OR bon.bonus IS NULL
+-- Write your PostgreSQL query statement below
+SELECT e.name, b.bonus FROM Employee e LEFT JOIN Bonus b ON e.empId = b.empId WHERE b.bonus < 1000 OR bonus is null;

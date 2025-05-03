24a916b3b6ee922bd18c5a48988f0b5c45b92203
Tarun <53268025+REDDITARUN@users.noreply.github.com>
2025-05-03 11:11:35 -0400

    Time: 1103 ms (98.03%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,4 +1,5 @@
-# Write your MySQL query statement below
-SELECT prod.product_name, sal.year, sal.price 
-FROM Sales sal
-LEFT JOIN Product prod ON sal.product_id = prod.product_id
+-- Write your PostgreSQL query statement below
+
+SELECT s.year, s.price, p.product_name 
+FROM Product p INNER JOIN Sales s
+ON p.product_id = s.product_id;

d64f395588fd216ced20ec0a21e0f8f8de302758
Tarun <53268025+REDDITARUN@users.noreply.github.com>
2025-05-04 14:16:26 -0400

    Time: 261 ms (49.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+employee_id, department_id
+FROM Employee WHERE primary_flag = 'Y'
+OR employee_id IN (SELECT employee_id FROM employee GROUP BY employee_id HAVING COUNT(employee_id) = 1)
